7f2203642827a6880f801f7579e043f1853d8170
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2023-12-13 18:35:59 -0500

    Update hic2cool.sh

--- hic2cool.sh
+++ hic2cool.sh
@@ -10,7 +10,17 @@ outdir=$(awk "NR==${SLURM_ARRAY_TASK_ID} {print \$2}" input.txt)
 
 cd ${outdir}
 
+# .hic to .txt
 module load python/cpu/3.8.11 
 module load gcc/8.1.0
 
 python hic2cool.py
+
+# .txt to .cool
+module unload python/cpu/3.8.11 
+module unload gcc/8.1.0
+
+module load anaconda3/cpu/5.2.0
+conda activate hicexplorer
+
+cooler load -f bg2 hg38.size:10000 matrix.txt matrix.cool
