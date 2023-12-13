d5b954c8abab200f7a9235b9f3d2d9d745b96750
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2023-12-13 18:31:58 -0500

    Update hic2cool.sh

--- hic2cool.sh
+++ hic2cool.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -J hic2cool
-#SBATCH --mem=10G
-#SBATCH --time=4:00:00
+#SBATCH --mem=40G
+#SBATCH --time=24:00:00
 #SBATCH -N 1
 #SBATCH -c 1
 
@@ -9,6 +9,8 @@ fileName=$(awk "NR==${SLURM_ARRAY_TASK_ID} {print \$1}" input.txt)
 outdir=$(awk "NR==${SLURM_ARRAY_TASK_ID} {print \$2}" input.txt)
 
 cd ${outdir}
-module load python
+
+module load python/cpu/3.8.11 
+module load gcc/8.1.0
 
 python hic2cool.py
