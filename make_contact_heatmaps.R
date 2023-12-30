b730e68d5deee99533c480d48b002aa5b56c91e9
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2023-12-30 16:27:13 -0500

    Update and rename plot_contact_hmap.R to make_contact_heatmaps.R

---
+++ make_contact_heatmaps.R
@@ -0,0 +1,64 @@
+# functions
+plotMat=function(data,start,end,max_distance,resolution){
+  res=resolution
+  s=ceiling(start/res)
+  e=ceiling(end/res)
+  max=max_distance/res
+  
+  x=as.matrix(log(data[s:e,s:e]+1))
+  x=rotateMatrix(x, -45)
+  c=e/2
+  x1=x[(c-max):c,]
+  coord=paste0(chr,"_",start,"-",end)
+  png(paste0("contact_hmap_",coord,".png"),width = 1920, height = 240)
+  print(pheatmap(x1,main=paste0("Coordinates: ",chr,":",start,"-",end," (bp); Max distance: ",max_distance, " (bp)"),
+           cluster_rows = F,cluster_cols = F,
+           show_rownames = F,show_colnames = F,
+           inferno(10)))
+  dev.off()
+}
+
+# libraries
+library(pheatmap)
+library(reticulate)
+library(RColorBrewer)
+library(viridis)
+library(omnibus)
+
+# Parameters
+start = 1
+end = 46000000
+resolution = 10000
+max_distance = 2000000 
+chromosomes = c(paste0("chr",1:22))
+options(scipen = 999)
+np <- import("numpy")
+
+chromosomes="chr10"
+for (chr in chromosomes){
+  print(chr)
+  # data reading
+  file = paste0(chr,".npz")
+  m <- np$load(file)
+  N = length(m['0'])
+  mat = matrix(data=0,nrow = N+1,ncol = N+1)
+
+  # create matrix
+  for (ii in as.character(m$files)){
+    jv = m$f[[ii]]
+    i = as.numeric(ii)
+    for (j in 1:dim(jv)){
+      if(i>=0){
+        mat[j,i+j] = jv[j]
+      }
+      if(i<0){
+        mat[abs(i)+j,j] = jv[j]
+      }
+    }
+  }    
+  M=as.data.frame(mat)
+
+  # plot
+  plotMat(M,start,end,max_distance,resolution)
+}
+
